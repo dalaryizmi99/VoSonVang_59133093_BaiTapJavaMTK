5a47ec6a3fa6d6614ab83ca96218d11822372ae0
Dalaryizmi <vang12383@gmail.com>
2020-04-13 19:22:56 +0700

    Complete task1

--- src/vosonvang_59133093_baitapjavamtk/VoSonVang_59133093_BaiTapJavaMTK.java
+++ src/vosonvang_59133093_baitapjavamtk/VoSonVang_59133093_BaiTapJavaMTK.java
@@ -17,6 +17,21 @@ public class VoSonVang_59133093_BaiTapJavaMTK {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        NhanVien nv1 = new NhanVien();
+        nv1.setTen("Vo Son Vang");
+        nv1.setTuoi(21);
+        nv1.setDiaChi("01 Ha Hoang, Ha Tinh");
+        nv1.setTienLuong(25000000);
+        nv1.setSumGioLam(201);
+        System.out.println(nv1.getThongTin());
+        
+        NhanVien nv2 = new NhanVien();
+        nv2.setTen("Vo Huu Huy");
+        nv2.setTuoi(21);
+        nv2.setDiaChi("Ninh Hoa, Khanh Hoa");
+        nv2.setTienLuong(25000000);
+        nv2.setSumGioLam(90);
+        System.out.println(nv2.getThongTin());
     }
     
 }
