04a2e407cba1338161d9f53162c2083ee9c0bed8
Dalaryizmi <vang12383@gmail.com>
2020-04-13 17:19:56 +0700

    First

---
+++ src/vosonvang_59133093_baitapjavamtk/NhanVien.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vosonvang_59133093_baitapjavamtk;
+
+/**
+ *
+ * @author Dalaryizmi
+ */
+public class NhanVien {
+    private String ten;
+    private int tuoi;
+    private String diachi;
+    private double tienluong;
+    private int sumgiolam;
+}
+
+public NhanVien(){
+    
+}
+
+public String getThongTin(){
+    
+}
+
+public double tinhThuong(){
+    if()
+}
---
+++ src/vosonvang_59133093_baitapjavamtk/VoSonVang_59133093_BaiTapJavaMTK.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vosonvang_59133093_baitapjavamtk;
+
+/**
+ *
+ * @author Dalaryizmi
+ */
+    
+public class VoSonVang_59133093_BaiTapJavaMTK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
